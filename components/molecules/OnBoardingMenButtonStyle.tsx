553583efb61b845f41bee3d58a12512bd8af2ef2
doodshot <guido.panzieri@edu.itspiemonte.it>
2024-12-05 21:58:52 +0100

    Reapply "quasi finito prima pagina di OnBoarding e iniziato navigation per la seconda pagina"
    This reverts commit ef8356643870300b2623ba6002d1ff414c136352.

---
+++ components/molecules/OnBoardingMenButtonStyle.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+ const styles = StyleSheet.create({
+     container: {
+        borderWidth: 1,
+         marginTop: 10,
+         backgroundColor: '#FFFFFF40',
+        height: 44,
+         justifyContent: 'center',
+         width: 200,
+         alignItems: 'center',
+         borderRadius: 20,
+         borderColor: '#FFFFFF',
+     },
+     title: {
+         fontSize: 16,
+         textAlign: "center",
+         color: "#FFF",
+         fontFamily: 'Poppins',
+         fontWeight: 'bold',
+     }
+})
+
+export default styles
